31a0a8e7a676268089a9076a3032934d34f4946e
Andrew <andrew.s.creegan@gmail.com>
2022-05-13 11:23:21 +1200

    Adding more boilerplate docs and build files for github pages

---
+++ docs/authors.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Andrew Creegan <andrew.s.creegan@gmail.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'Andrew Creegan'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,3 +49,24 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = ['css/custom.css']
+
+html_theme_options = {
+    "sidebar_width": "250px",
+    "page_width": "95%"
+}
+
+autodoc_default_options = {
+    'member-order': 'bysource'
+}
+
+autodoc_type_aliases = {
+    'Iterable': 'Iterable',
+    'ArrayLike': 'ArrayLike'
+}
+
+autodoc_typehints = "description"
+autodoc_typehints_format = "short"
+autodoc_typehints_description_target = "documented"
+napoleon_use_rtype = False
